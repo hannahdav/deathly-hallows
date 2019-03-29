f85cf6e84e80ad49933f53d784b845a7da3005ad
Nic Gibson <nicgib1@gmail.com>
2019-03-29 13:57:40 -0600

    added more bios

--- DeathlyDating/src/models/bios.js
+++ DeathlyDating/src/models/bios.js
@@ -35,6 +35,9 @@ const bios = [
     'Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend.',
     'Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying.',
     'The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did.',
-    'Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.'
+    'Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.',
+    'I Am Not A Human And I Don't Speak English',
+    'How can mirrors be real if our eyes aren’t real.',
+    'The only way to change something is to shock it. If you want your muscles to grow, you have to shock them. If you want society to change, you have to shock them.'
 ];
 export default bios;
